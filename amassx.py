8b1fc5dd8c499412a78c42efc4c52b08de55190f
Tonmoy9518 <137045151+Tonmoy9518@users.noreply.github.com>
2023-09-22 22:49:48 +0600

    Update amassx.py

--- amassx.py
+++ amassx.py
@@ -3,6 +3,8 @@
 import subprocess
 import time
 import httpx
+import os
+import shutil
 
 def animate_running():
     animation = "|/-\\"
@@ -39,21 +41,44 @@ def collect_urls(subdomain_file):
         for url in all_urls:
             all_output_file.write(url + "\n")
 
+def install_amassx():
+    # Get the current script's location
+    script_location = os.path.abspath(__file__)
+    script_name = os.path.basename(script_location)
+
+    # Destination directory for installation
+    destination_directory = '/usr/local/bin'
+
+    try:
+        # Copy the script to the destination directory
+        shutil.copy(script_location, os.path.join(destination_directory, script_name))
+
+        # Make the script executable
+        os.chmod(os.path.join(destination_directory, script_name), 0o755)
+
+        print(f"{script_name} has been installed to {destination_directory}/{script_name}")
+    except Exception as e:
+        print(f"Installation failed: {e}")
+
 if __name__ == "__main__":
     animation_stopped = False  # Flag to control the animation
     domain = input("Enter Your Target: ")
 
-    # Start the animation in a separate thread
-    animation_thread = threading.Thread(target=animate_running)
-    animation_thread.start()
+    # Check if the script is being run for installation
+    if domain.lower() == "install":
+        install_amassx()
+    else:
+        # Start the animation in a separate thread
+        animation_thread = threading.Thread(target=animate_running)
+        animation_thread.start()
 
-    subprocess.call(f"amass enum -passive -d {domain} -o sub_{domain}.txt", shell=True)
-    stop_animation()  # Stop the animation after "AMASS END" is displayed
-    animation_thread.join()  # Wait for the animation thread to finish
+        subprocess.call(f"amass enum -passive -d {domain} -o sub_{domain}.txt", shell=True)
+        stop_animation()  # Stop the animation after "AMASS END" is displayed
+        animation_thread.join()  # Wait for the animation thread to finish
 
-    print("\nAMASS END")
+        print("\nAMASS END")
 
-    print("Collecting all URLs and checking for live URLs...")
-    collect_urls(f"sub_{domain}.txt")
-    print("Live URLs collected in live_url.txt")
-    print("All URLs collected in all_url.txt")
+        print("Collecting all URLs and checking for live URLs...")
+        collect_urls(f"sub_{domain}.txt")
+        print("Live URLs collected in live_url.txt")
+        print("All URLs collected in all_url.txt")
