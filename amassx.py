1a6558cbe55b27aed85c566cba4f3f0f7b983580
Tonmoy9518 <137045151+Tonmoy9518@users.noreply.github.com>
2023-09-22 22:58:39 +0600

    Update amassx.py

--- amassx.py
+++ amassx.py
@@ -5,6 +5,7 @@ import time
 import httpx
 import os
 import shutil
+import sys
 
 def animate_running():
     animation = "|/-\\"
@@ -41,35 +42,45 @@ def collect_urls(subdomain_file):
         for url in all_urls:
             all_output_file.write(url + "\n")
 
+def install_amassx():
+    # Get the current script's location
+    script_location = os.path.abspath(__file__)
+    script_name = os.path.basename(script_location)
+    destination_directory = '/usr/local/bin'
+
+    try:
+        # Copy the script to the destination directory
+        shutil.copy(script_location, os.path.join(destination_directory, script_name))
+
+        # Make the script executable
+        os.chmod(os.path.join(destination_directory, script_name), 0o755)
+
+        print(f"{script_name} has been installed to {destination_directory}/{script_name}")
+        sys.exit(0)  # Exit after successful installation
+    except Exception as e:
+        print(f"Installation failed: {e}")
+        sys.exit(1)  # Exit with an error code
+
 if __name__ == "__main__":
     animation_stopped = False  # Flag to control the animation
 
-    # Installation logic
-    if not os.path.exists("/usr/local/bin/amassx.py"):
-        script_location = os.path.abspath(__file__)
-        script_name = os.path.basename(script_location)
-        destination_directory = '/usr/local/bin'
+    # Check if the script is being run for installation
+    if len(sys.argv) > 1 and sys.argv[1].lower() == "install":
+        install_amassx()
 
-        try:
-            shutil.copy(script_location, os.path.join(destination_directory, script_name))
-            os.chmod(os.path.join(destination_directory, script_name), 0o755)
-            print(f"{script_name} has been installed to {destination_directory}/{script_name}")
-        except Exception as e:
-            print(f"Installation failed: {e}")
-    else:
-        domain = input("Enter Your Target: ")
+    domain = input("Enter Your Target: ")
 
-        # Start the animation in a separate thread
-        animation_thread = threading.Thread(target=animate_running)
-        animation_thread.start()
+    # Start the animation in a separate thread
+    animation_thread = threading.Thread(target=animate_running)
+    animation_thread.start()
 
-        subprocess.call(f"amass enum -passive -d {domain} -o sub_{domain}.txt", shell=True)
-        stop_animation()  # Stop the animation after "AMASS END" is displayed
-        animation_thread.join()  # Wait for the animation thread to finish
+    subprocess.call(f"amass enum -passive -d {domain} -o sub_{domain}.txt", shell=True)
+    stop_animation()  # Stop the animation after "AMASS END" is displayed
+    animation_thread.join()  # Wait for the animation thread to finish
 
-        print("\nAMASS END")
+    print("\nAMASS END")
 
-        print("Collecting all URLs and checking for live URLs...")
-        collect_urls(f"sub_{domain}.txt")
-        print("Live URLs collected in live_url.txt")
-        print("All URLs collected in all_url.txt")
+    print("Collecting all URLs and checking for live URLs...")
+    collect_urls(f"sub_{domain}.txt")
+    print("Live URLs collected in live_url.txt")
+    print("All URLs collected in all_url.txt")
